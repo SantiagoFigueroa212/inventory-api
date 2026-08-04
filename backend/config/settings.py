f1f41b46757ec9c299e8d7d3171ffb6f858960a7
Santiago <santiagofigueroa470@gmail.com>
2026-08-04 17:20:45 -0600

    configuracnion de llave

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR.parent / ".env")
 
 
 # Quick-start development settings - unsuitable for production
